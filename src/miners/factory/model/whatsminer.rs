8ec0d2cf3cce96d3b530b6cc37f861ed0beebfe8
s0kil <dtechtours@gmail.com>
2025-05-30 15:19:53 -0400

    Remove Deserializer From get_model_whatsminer_v3

--- src/miners/factory/model/whatsminer.rs
+++ src/miners/factory/model/whatsminer.rs
@@ -2,7 +2,6 @@ use crate::data::device::models::MinerModelFactory;
 use crate::data::device::{MinerMake, MinerModel};
 use crate::miners::api::rpc::{btminer::BTMinerV3RPC, traits::SendRPCCommand};
 use crate::miners::util;
-use serde::{Deserialize, Deserializer};
 use serde_json::Value;
 use std::net::IpAddr;
 
@@ -26,38 +25,21 @@ pub(crate) async fn get_model_whatsminer_v2(ip: IpAddr) -> Option<MinerModel> {
     }
 }
 
-#[derive(Debug)]
-pub struct GetDeviceMinerInfo {
-    pub model: Option<String>,
-}
-
-impl<'de> Deserialize<'de> for GetDeviceMinerInfo {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let value: Value = Value::deserialize(deserializer)?;
-
-        let model = value["msg"]["miner"]["type"]
-            .as_str()
-            .map(|s| s.to_string());
-
-        Ok(Self { model })
-    }
-}
-
 pub(crate) async fn get_model_whatsminer_v3(ip: IpAddr) -> Option<MinerModel> {
     let rpc = BTMinerV3RPC::new(ip, None);
-    let device_miner_info = rpc
-        .send_command::<GetDeviceMinerInfo, &str>("get.device.info", Some("miner"))
+    let response = rpc
+        .send_command::<Value, &str>("get.device.info", Some("miner"))
         .await;
 
-    match device_miner_info {
-        Ok(data) => {
-            if data.model.is_none() {
+    match response {
+        Ok(json_data) => {
+            let model = json_data["msg"]["miner"]["type"].as_str();
+
+            if model.is_none() {
                 return None;
             }
-            let mut model = data.model.unwrap().to_uppercase().replace("_", "");
+
+            let mut model = model.unwrap().to_uppercase().replace("_", "");
             model.pop();
             model.push('0');
 
